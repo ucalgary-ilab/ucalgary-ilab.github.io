5c2dfa3df8981e734d07e4e11b1df2d1f3390ea8
ChristianFrisson <ChristianFrisson@users.noreply.github.com>
2025-09-19 23:10:16 +0000

    Deploy preview for PR 86 🛫

--- pr-preview/pr-86/_next/static/chunks/app/publication/page-dc99fab78e41e894.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[531],{77193:(e,s,_)=>{Promise.resolve().then(_.bind(_,71317))}},e=>{e.O(0,[935,619,216,491,114,317,441,255,358],()=>e(e.s=77193)),_N_E=e.O()}]);
--- pr-preview/pr-86/_next/static/chunks/app/publications/[slug]/page-563a3e1d0cece6f4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[932],{40528:(e,s,_)=>{Promise.resolve().then(_.bind(_,71317))}},e=>{e.O(0,[935,619,216,491,114,317,441,255,358],()=>e(e.s=40528)),_N_E=e.O()}]);
