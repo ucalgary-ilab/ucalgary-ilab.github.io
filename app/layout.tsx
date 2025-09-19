f2a5e69e705b5f6040b785478a9948c4a998ec27
Christian Frisson <christian.frisson@gmail.com>
2025-09-19 11:11:46 -0600

    Fix google font next.js integration
    https://nextjs.org/docs/app/getting-started/fonts#google-fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,13 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
+/* google fonts https://nextjs.org/docs/app/getting-started/fonts#google-fonts */
+import { Open_Sans } from 'next/font/google'
+const openSans = Open_Sans({
+  subsets: ['latin'],
+  weight: ['400','700'],
+})
+
 /* global */
 import './global.css'
 
@@ -16,15 +23,13 @@ export default function RootLayout({
     children: React.ReactNode
   }) {
     return (
-        <html>
+        <html lang="en" className={openSans.className}>
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta name="format-detection" content="telephone=no" />
 
-          <link href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet" />
           <link href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.0/semantic.css" rel="stylesheet" />
           <link href="/assets/img/favicon.ico" rel="shortcut icon" />
           {/* <link href="/static/css/style.css" rel="stylesheet" /> */}
