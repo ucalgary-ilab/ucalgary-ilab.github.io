cc893b9ee62d4562166cdeba0b24bead05c10356
ChristianFrisson <ChristianFrisson@users.noreply.github.com>
2025-12-17 16:19:56 +0000

    Deploy preview for PR 138 🛫

--- pr-preview/pr-138/_next/static/chunks/vendor-styles-26b30cb322f4d5ea.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[308],{717:()=>{},2330:()=>{},8296:a=>{a.exports={style:{fontFamily:"'Encode Sans', 'Encode Sans Fallback'",fontStyle:"normal"},className:"__className_bae0a2"}}}]);
