c8f1e594c42a13e68d06da5a51e69c53e321a460
ChristianFrisson <christian.frisson@gmail.com>
2025-12-17 16:18:12 +0000

    Deploying to gh-pages from @ ucalgary-ilab/ucalgary-ilab.github.io@1d99f12718fb0a23d7e5d0c276a31bed6f9c197f 🚀

---
+++ _next/static/chunks/vendor-styles-25e9cf206ffc5a28.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[308],{30717:()=>{},42330:()=>{},78296:a=>{a.exports={style:{fontFamily:"'Encode Sans', 'Encode Sans Fallback'",fontStyle:"normal"},className:"__className_7d1f77"}}}]);
